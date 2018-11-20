12f73f1f3735fb96a2804ebef5076ba32360e7d7
Guillaume Cabanel <guillaume.cabanel@gmail.com>
2018-11-20 18:13:07 +0100

    visitor can add a lost pet

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 ## Features
 [X] Visitor can list all missing pets
 [X] Visitor can see a missing pet
-[ ] Visitor can add a missing pet
+[X] Visitor can add a missing pet
 [ ] Visitor can mark a pet as found
 [ ] Visitor can remove a pet from the list
--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,4 +6,23 @@ class PetsController < ApplicationController
   def show
   	@pet = Pet.find(params[:id])
   end
+
+  def new
+  	@pet = Pet.new
+  end
+
+  def create
+  	Pet.create(pet_params)
+  	redirect_to pets_path
+  end
+
+  private
+
+  def pet_params
+  	params.require(:pet).permit(
+  		:name,
+  		:specy,
+  		:sign
+  	)
+  end
 end
